c20d86967f4bdf487744bc0231e38c0d0c6da144
Nukol <114868796+NukolLodda@users.noreply.github.com>
2022-11-05 20:20:09 -0500

    Add files via upload

---
+++ wtf.py
@@ -0,0 +1,144 @@
+import math
+import random
+
+def p_man(sex):
+    while not(sex == 112):
+        sex = random.randint(97,122)
+    return sex
+
+def enis(hump_me,fuck_me,daddy_better,make_me_scream):
+    while hump_me != 4000:
+        if hump_me < 4000:
+            hump_me += 1
+        else:
+            if hump_me > 4000:
+                hump_me -= 1
+    while fuck_me != 700:
+        if fuck_me % 100 != 0:
+            x = fuck_me % 100
+            y = 100 - x
+            fuck_me += y
+        else:
+            if fuck_me < 700:
+                fuck_me += ((math.sqrt(25) + 256 ** (1/math.sqrt(69 - 5))) ** 2)
+            elif fuck_me > 700:
+                fuck_me -= math.sqrt(4761) + (420 - 389)
+    while daddy_better != 60:
+        daddy_better //= 69
+        if daddy_better != 60:
+            daddy_better //= 69
+            if daddy_better != 60:
+                if daddy_better > 60:
+                    sixtynine = -69420 + 69419
+                    absof69 = abs(sixtynine)
+                    daddy_better += -(absof69)
+                elif daddy_better < 60:
+                    hundredmod = 100 % 69
+                    hundredmod += 18
+                    sqrtthing = hundredmod ** 0.5
+                    sqrtthing //= 2
+                    daddy_better = sqrtthing + 57
+    while make_me_scream != 1: 
+        make_me_scream = 1
+    hump_me_fuck_me = "my tunnel loves a deep throat"
+    lick_lick_lick_lick_I_wanna_eat_yo_dick = hump_me + fuck_me + daddy_better + make_me_scream
+    but_I_cant_fuck_cause_of_my_nails = math.sqrt(lick_lick_lick_lick_I_wanna_eat_yo_dick)
+    so_Imma_pick_it_up_with_chopsticks = 64 ** 0.5
+    mouth_wide_open_mouth_wide_open = (so_Imma_pick_it_up_with_chopsticks / 2)
+    mouth_wide_open_like_Im_at_the_dentist = 4067 + len(hump_me_fuck_me)
+    mouth_wide_open_mouth_wide_open = mouth_wide_open_mouth_wide_open ** 2
+    put_it_so_deep_I_cant_speak_a_sentence = (mouth_wide_open_like_Im_at_the_dentist ** (1/12)) * but_I_cant_fuck_cause_of_my_nails
+    return (put_it_so_deep_I_cant_speak_a_sentence - 37) // 1
+
+def cpr(yo_dick,brick_hard,like_a_metal):
+    yo_dick = 10 ** 0
+    brick_hard = 10 ** 1
+    like_a_metal = 10 ** 2
+    tight_as_a_virgin = yo_dick - 1
+    yeah_boy_dont_get_nervous = brick_hard + like_a_metal
+    Im_here_to_serve_you = tight_as_a_virgin + yeah_boy_dont_get_nervous
+    customer_service = Im_here_to_serve_you - 110
+    if customer_service == 0:
+        I_save_dick_by_giving_it_cpr = 117
+        dick = math.sqrt(81)
+        cock = math.sqrt(dick + customer_service)
+        put_your_mouth_in = (I_save_dick_by_giving_it_cpr * cock) + 69
+    else:
+        I_save_dick_by_giving_it_cpr = 117
+        dick = math.sqrt(81)
+        cock = math.sqrt(dick + 0)
+        put_your_mouth_in = (I_save_dick_by_giving_it_cpr * cock) + 69
+    square17 = 17 ** 2
+    formycock = math.sqrt(49)
+    formyothercock = math.sqrt(9)
+    ultracock = formycock * formyothercock
+    it_like_cpr = square17 + ultracock
+    return (put_your_mouth_in - it_like_cpr) // 1
+
+def imaginery_piss(ass):
+    vagina = 0
+    for i in range(ass):
+        vagina += 1
+    if vagina == 105:
+        return vagina // 1
+    else:
+        vagina //= 69
+        wap = vagina % 69
+        vagina += wap
+        line1 = "Now from the top, make it drop, that's some wet ass pussy"
+        line2 = "Now grab a bucket, and a mop, that's some wet ass pussy"
+        line3 = "I'm talking wap wap wap, that's some wet ass pussy"
+        line4 = "Macaronit in a pot, that's some wet ass pussy"
+        line1 = len(line1)
+        line2 = len(line2)
+        line3 = len(line3)
+        line4 = len(line4)
+        line1 += line2
+        line3 += line4
+        line1 -= line3
+        line1 += 9 ** 2
+        vagina += math.sqrt(25)
+        vagina += line1
+        return (vagina - 10) // 1
+
+def penis_length(girth):
+    angle = 45
+    length = 0.0001
+    tmi = random.randint(1,2)
+    if tmi == 1:
+        girth //= angle
+    elif tmi == 2:
+        girth += length * 10 ** 4
+    for i in range(69420):
+        if girth == 115:
+            break
+        else:
+            while girth > 115:
+                girth += 1
+                girth -= math.sqrt(4)
+            while girth < 115:
+                girth += 69
+    return girth // 1
+
+def chrification(val1,val2,val3,val4,val5):
+    chrified = []
+    chrified.append(chr(val1))
+    chrified.append(chr(val2))
+    chrified.append(chr(val3))
+    chrified.append(chr(val4))
+    chrified.append(chr(val5))
+    return chrified
+
+def main():
+    dickhole = chrification(
+        p_man(69),
+        int(enis(0,6,9,0)),
+        int(cpr(4,2,0)),
+        int(imaginery_piss(420)),
+        int(penis_length(89))
+    )
+    print("".join(dickhole))
+    input()
+
+if __name__ == "__main__":
+    main()
